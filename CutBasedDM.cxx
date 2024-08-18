3f8b8d72e665e6d2c68f02758a9d24cd8b4b0d15
Tamas Vami <tamas.almos.vami@cern.ch>
2024-08-18 10:20:50 -0700

    Add version 6

--- CutBasedDM.cxx
+++ CutBasedDM.cxx
@@ -4,12 +4,17 @@
 #include "Hcal/Event/HcalVetoResult.h"
 #include "DetDescr/HcalID.h"
 #include "SimCore/Event/SimParticle.h"
+#include "DetDescr/SimSpecialID.h"
+#include "SimCore/Event/SimTrackerHit.h"
+#include <math.h>
 
 // v0: Just a few cuts, establish minimal scenario
 // v1: More strict cuts
 // v2: Fix for N-1 plots, fix NReadoutHits binning
 // v3: Max cell dep is not depth but deposition, non-fid option
 // v4: Adding HCAL PE variables, change to truth level recoil ele momentum info
+// v5: Adding Target SP recoil ele variables, trigger eff, Target SP angles
+// v6: Add alternative cutflow where fiducial is before trigger, add BDT score (Gabrielle), BDT vs PE
 
 class CutBasedDM : public framework::Analyzer {
 public:
@@ -21,7 +26,7 @@ public:
   void analyze(const framework::Event& event) final;
   template <typename T, size_t n>
   bool passPreselection(T (&passedCutsArray)[n], bool verbose);
-  std::tuple<int, const ldmx::SimParticle *> getRecoil(
+  std::tuple<int, const ldmx::SimParticle *> getRecoilEle(
     const std::map<int, ldmx::SimParticle> &particleMap);
   std::string trigger_collName_;
   std::string trigger_passName_;
@@ -39,8 +44,11 @@ void CutBasedDM::configure(framework::config::Parameters &ps) {
 
 void CutBasedDM::onProcessStart(){
   getHistoDirectory();
-  
-  histograms_.create("RecoilX", "", 20, -0.5, 19.5, "RecoilX [mm]", 90, -450.0, 450.0);
+
+  histograms_.create("TrigEffVsMissingE", "Triggered?", 2, -0.5, 1.5, "Missing ECAL energy [MeV]", 100, 0.0, 8000.0);
+  histograms_.create("TrigEffVsRecoilPTAtTarget", "Triggered?", 2, -0.5, 1.5, "Recoil p_{T} @Target SP [MeV]", 800, 0.0, 8000.0);
+  histograms_.create("RecoilX", "", 20, -0.5, 19.5, "RecoilX @Ecal SP [mm]", 90, -450.0, 450.0);
+  histograms_.create("RecoilXAtTarget", "", 20, -0.5, 19.5, "RecoilX @Target SP [mm]", 90, -450.0, 450.0);
   histograms_.create("AvgLayerHit", "", 20, -0.5, 19.5, "Avg hit layer", 35, -0.5, 34.5);
   histograms_.create("DeepestLayerHit", "", 20, -0.5, 19.5, "Deepest hit layer", 35, -0.5, 34.5);
   histograms_.create("EcalBackEnergy", "", 20, -0.5, 19.5, "Ecal back energy [MeV]", 100, 0.0, 3000.0);
@@ -57,14 +65,31 @@ void CutBasedDM::onProcessStart(){
   histograms_.create("ShowerRMS", "", 20, -0.5, 19.5, "Shower RMS [mm]", 100, 0.0, 250.0);
   histograms_.create("XStd", "", 20, -0.5, 19.5, "Shower RMS_{X} [mm]", 100, 0.0, 250.0);
   histograms_.create("YStd", "", 20, -0.5, 19.5, "Shower RMS_{Y} [mm]", 100, 0.0, 250.0);
+  histograms_.create("BDTDiscr", "", 20, -0.5, 19.5, "BDT discriminating score", 100, 0.0, 1.0);
+  histograms_.create("BDTDiscrLog", "", 20, -0.5, 19.5, "-log(1-BDT discriminating score)", 100, 0.0, 5.0);
+  
   histograms_.create("RecoilPT", "", 20, -0.5, 19.5, "Recoil p_{T} [MeV]", 800, 0.0, 8000.0);
   histograms_.create("RecoilPZ", "", 20, -0.5, 19.5, "Recoil p_{Z} [MeV]", 800, -10.0, 8010.0);
   histograms_.create("RecoilP", "", 20, -0.5, 19.5, "Recoil p [MeV]", 800, 0.0, 8000.0);
+  histograms_.create("RecoilPTAtTarget", "", 20, -0.5, 19.5, "Recoil p_{T} @Target SP [MeV]", 800, 0.0, 8000.0);
+  histograms_.create("RecoilPZAtTarget", "", 20, -0.5, 19.5, "Recoil p_{Z} @Target SP [MeV]", 800, -10.0, 8010.0);
+  histograms_.create("RecoilPAtTarget", "", 20, -0.5, 19.5, "Recoil p @Target SP [MeV]", 800, 0.0, 8000.0);
+  histograms_.create("RecoilTheta", "", 20, -0.5, 19.5, "Recoil theta @Target SP", 90, 0.0, 90.0);
+  histograms_.create("RecoilPhi", "", 20, -0.5, 19.5, "Recoil phi @Target SP", 360, -180.0, 180.0);
+
 
   histograms_.create("Hcal_MaxPE", "", 20, -0.5, 19.5, "HCAL max photo-electron hits", 65, -0.5, 64.5);
   histograms_.create("Hcal_TotalPE", "", 20, -0.5, 19.5, "HCAL total photo-electron hits", 100, -0.5, 200.5);
   histograms_.create("Hcal_MaxTiming", "", 20, -0.5, 19.5, "HCAL timing of the max PE hit", 35, 0.0, 35.0);
   histograms_.create("Hcal_MaxSector", "", 20, -0.5, 19.5, "", 5, -0.5, 4.5);
+
+  histograms_.create("BDTDiscrVsHcalPE_PreS", "HCAL PE", 500, -0.5, 500.5, "BDT discriminating score", 100, 0.0, 1.0);
+  histograms_.create("BDTDiscrLogVsHcalPE_PreS", "HCAL PE", 500, -0.5, 500.5, "-log(1-BDT discriminating score)", 100, 0.0, 5.0);
+  histograms_.create("BDTDiscrVsHcalPE_PostS", "HCAL PE", 500, -0.5, 500.5, "BDT discriminating score", 100, 0.0, 1.0);
+  histograms_.create("BDTDiscrLogVsHcalPE_PostS", "HCAL PE", 500, -0.5, 500.5, "-log(1-BDT discriminating score)", 100, 0.0, 5.0);
+
+  histograms_.create("AltCutFlow_RecoilX", "", 20, -0.5, 19.5, "RecoilX @Ecal SP [mm]", 90, -450.0, 450.0);
+  
 
 // Reverse direction
   histograms_.create("Rev_RecoilX", "", 20, -0.5, 19.5, "RecoilX [mm]", 90, -450.0, 450.0);
@@ -104,6 +129,8 @@ void CutBasedDM::onProcessStart(){
   histograms_.create("N1_Hcal_MaxTiming", "", 20, -0.5, 19.5, "HCAL timing of the max PE hit", 35, 0.0, 35.0);
   histograms_.create("N1_Hcal_MaxSector", "", 20, -0.5, 19.5, "", 5, -0.5, 4.5);
 
+  // histograms_.create("N1_EcalBackEnergy", "", 20, -0.5, 19.5, "Ecal back energy [MeV]", 100, 0.0, 3000.0);
+
   std::vector<std::string> labels = {
     "All",
     "Triggerred",            // 1
@@ -139,10 +166,19 @@ void CutBasedDM::onProcessStart(){
   histograms_.get("ShowerRMS"),
   histograms_.get("XStd"),
   histograms_.get("YStd"),
+  histograms_.get("BDTDiscr"),
+  histograms_.get("BDTDiscrLog"),
+
   histograms_.get("RecoilX"),
   histograms_.get("RecoilPT"),
   histograms_.get("RecoilPZ"),
   histograms_.get("RecoilP"),
+  histograms_.get("RecoilXAtTarget"),
+  histograms_.get("RecoilPTAtTarget"),
+  histograms_.get("RecoilPZAtTarget"),
+  histograms_.get("RecoilPAtTarget"),
+  histograms_.get("RecoilTheta"),
+  histograms_.get("RecoilPhi"),
   histograms_.get("Hcal_MaxPE"),
   histograms_.get("Hcal_TotalPE"),
   histograms_.get("Hcal_MaxTiming"),
@@ -234,6 +270,34 @@ void CutBasedDM::onProcessStart(){
       hist->GetYaxis()->SetBinLabel(ilabel, labels_HCALsector[ilabel - 1].c_str());
     }
   }
+
+    std::vector<TH1 *> hists_AltCutFlow = {
+    histograms_.get("AltCutFlow_RecoilX"),
+  };
+
+  // enum HcalSection { BACK = 0, TOP = 1, BOTTOM = 2, RIGHT = 3, LEFT = 4 };
+    std::vector<std::string> labels_AltCutFlow = {
+    "All",
+    "Fiducial",              // 1
+    "Triggerred",            // 2
+    "E_{sum} < 3500",       // 3
+    "E_{SumTight} < 800",        // 4
+    "E_{back} < 250",             // 5
+    "N_{hits} < 70",              // 6
+    "RMS_{shower} < 110",         // 7
+    "RMS_{shower,Y} < 70",        // 8
+    "E_{cell,max} < 300",     // 9
+    "RMS_{Layer,hit} < 5",        // 10
+    "N_{straight} < 6",           // 11
+    "PASS_{HCalVeto}",            // 12
+    ""};
+
+  for (int ilabel{1}; ilabel < labels_AltCutFlow.size(); ++ilabel) {
+    for (auto &hist : hists_AltCutFlow) {
+      hist->GetXaxis()->SetBinLabel(ilabel, labels_AltCutFlow[ilabel - 1].c_str());
+    }
+  }
+
 }
 
 void CutBasedDM::analyze(const framework::Event& event) {
@@ -242,28 +306,52 @@ void CutBasedDM::analyze(const framework::Event& event) {
   auto trigResult{event.getObject<ldmx::TriggerResult>(trigger_collName_, trigger_passName_)};
   auto hcalVeto{event.getObject<ldmx::HcalVetoResult>("HcalVeto","")};
   auto hcalRecHits{event.getCollection<ldmx::HcalHit>("HcalRecHits", "")};
+  auto targetSpHits{event.getCollection<ldmx::SimTrackerHit>("TargetScoringPlaneHits")};
 
-  // Recoil ele calc
-  float pT2{-9999.};
+  // Take recoil momentum from SIM particles
   float pT{-9999.};
   float pZ{-9999.};
-  float totMom2{-9999.};
   float totMom{-9999.};
+  auto particleMap{event.getMap<int, ldmx::SimParticle>("SimParticles")};
+  auto [recoilTrackID, recoilElectron] = CutBasedDM::getRecoilEle(particleMap);
+  pT =   sqrt(recoilElectron->getMomentum()[0] * recoilElectron->getMomentum()[0] +  recoilElectron->getMomentum()[1] * recoilElectron->getMomentum()[1]);
+  pZ = recoilElectron->getMomentum()[2];
+  totMom = sqrt(pT*pT + pZ*pZ);
+
+  // auto phiEle =   (180/M_PI)*std::acos(recoilElectron->getMomentum()[1] /totMom)-90.;
+  // auto thetaEle = (180/M_PI)*std::acos(pZ/totMom);
+
+  //  Same but at the target SP
+  float XAtTarget{-9999};
+  float pTAtTarget{-9999.};
+  float pYAtTarget{-9999.};
+  float pZAtTarget{-9999.};
+  float totMomAtTarget{-9999.};
+  for (ldmx::SimTrackerHit &spHit : targetSpHits) {
+    ldmx::SimSpecialID hit_id(spHit.getID());
+    if (hit_id.plane() != 1 || spHit.getMomentum()[2] <= 0) continue;
+
+    if (spHit.getTrackID() == recoilTrackID) {
+      float p_current = sqrt(spHit.getMomentum()[0]*spHit.getMomentum()[0] + spHit.getMomentum()[1]*spHit.getMomentum()[1] + spHit.getMomentum()[2]*spHit.getMomentum()[2]);
+      if (p_current > totMomAtTarget) {
+        pYAtTarget  = spHit.getMomentum()[1];
+        pTAtTarget = sqrt(spHit.getMomentum()[0]*spHit.getMomentum()[0] + spHit.getMomentum()[1]*spHit.getMomentum()[1]);
+        pZAtTarget = spHit.getMomentum()[2];
+        totMomAtTarget = p_current;
+        XAtTarget = spHit.getPosition()[0];
+      }
+    }
+  }
+//  std::cout << " pYAtTarget " <<  pYAtTarget << " totMomAtTarget " << totMomAtTarget ;
+  auto phiEleAtTarget =   (180/M_PI)*std::acos(pYAtTarget/totMomAtTarget)-90.;
+  auto thetaEleAtTarget = (180/M_PI)*std::acos(pZAtTarget/totMomAtTarget);
+  // std::cout << " phiEleAtTarget " << phiEleAtTarget << " phiEle " << phiEle
+  //  std::cout << " thetaEleAtTarget " << thetaEleAtTarget << " phiEleAtTarget " << phiEleAtTarget << std::endl;
 
   // Take recoil momentum from ECAL SP
   // pT2 = vetoNew.getRecoilMomentum()[0]*vetoNew.getRecoilMomentum()[0] + vetoNew.getRecoilMomentum()[1]*vetoNew.getRecoilMomentum()[1];
   // pZ =  vetoNew.getRecoilMomentum()[2];
 
-  // Take recoil momentum from SIM particles
-  auto particleMap{event.getMap<int, ldmx::SimParticle>("SimParticles")};
-  auto [recoilTrackID, recoilElectron] = CutBasedDM::getRecoil(particleMap);
-  pT2 =  recoilElectron->getMomentum()[0]* recoilElectron->getMomentum()[0] +  recoilElectron->getMomentum()[1]* recoilElectron->getMomentum()[1];
-  pZ = recoilElectron->getMomentum()[2];
-
-  if (pT2 >= 0) pT = sqrt(pT2);
-  if (pZ < 0 && pZ != -9999) pZ = -5;
-  if (pT2 >= 0) totMom2 = pT2 + pZ*pZ;
-  if (totMom2 >= 0) totMom = sqrt(totMom2);
 
   // HCAL veto calc
   float hcalMaxPE{-9999};
@@ -312,6 +400,10 @@ void CutBasedDM::analyze(const framework::Event& event) {
   hcalMaxTiming = maxPEHit->getTime();
   hcalMaxSector =  maxPeId.section();
 
+  // Trigger eff curves
+  histograms_.fill("TrigEffVsMissingE", trigResult.passed() , 8000.-vetoNew.getSummedDet() );
+  histograms_.fill("TrigEffVsRecoilPTAtTarget", trigResult.passed() , pTAtTarget );
+
   // CutFlow here
   bool passedCutsArray[12];
   std::fill(std::begin(passedCutsArray), std::end(passedCutsArray),false);
@@ -329,6 +421,7 @@ void CutBasedDM::analyze(const framework::Event& event) {
   passedCutsArray[10]  = (vetoNew.getNStraightTracks() < 6) ? true : false;
   passedCutsArray[11]  = (hcalVeto.passesVeto()) ? true : false;
 
+  // Fill histograms
   histograms_.fill("RecoilX", 0. , vetoNew.getRecoilX() );
   histograms_.fill("AvgLayerHit", 0. , vetoNew.getAvgLayerHit() );
   histograms_.fill("DeepestLayerHit", 0. , vetoNew.getDeepestLayerHit() );
@@ -346,13 +439,24 @@ void CutBasedDM::analyze(const framework::Event& event) {
   histograms_.fill("ShowerRMS", 0. , vetoNew.getShowerRMS() );
   histograms_.fill("XStd", 0. , vetoNew.getXStd() );
   histograms_.fill("YStd", 0. , vetoNew.getYStd() );
+  histograms_.fill("BDTDiscr", 0. , vetoNew.getDisc() );
+  histograms_.fill("BDTDiscrLog", 0. , -log(1-vetoNew.getDisc()) );
+  
   histograms_.fill("RecoilPT", 0. ,pT );
   histograms_.fill("RecoilPZ", 0. , pZ );
   histograms_.fill("RecoilP", 0. , totMom );
+  histograms_.fill("RecoilXAtTarget", 0. ,XAtTarget );
+  histograms_.fill("RecoilPTAtTarget", 0. ,pTAtTarget );
+  histograms_.fill("RecoilPZAtTarget", 0. , pZAtTarget );
+  histograms_.fill("RecoilPAtTarget", 0. , totMomAtTarget );
+  histograms_.fill("RecoilTheta", 0. , thetaEleAtTarget );
+  histograms_.fill("RecoilPhi", 0. , phiEleAtTarget );
   histograms_.fill("Hcal_MaxPE", 0. , hcalMaxPE );
   histograms_.fill("Hcal_TotalPE", 0. , hcalTotalPe );
   histograms_.fill("Hcal_MaxTiming", 0. , hcalMaxTiming );
   histograms_.fill("Hcal_MaxSector", 0. , hcalMaxSector );
+
+  histograms_.fill("AltCutFlow_RecoilX", 0 , vetoNew.getRecoilX() );
   
   for (size_t i=0;i<sizeof(passedCutsArray);i++) {
     bool allCutsPassedSoFar = true;
@@ -397,16 +501,79 @@ void CutBasedDM::analyze(const framework::Event& event) {
       histograms_.fill("ShowerRMS", i+1 , vetoNew.getShowerRMS() );
       histograms_.fill("XStd", i+1 , vetoNew.getXStd() );
       histograms_.fill("YStd", i+1 , vetoNew.getYStd() );
+      histograms_.fill("BDTDiscr", i+1 , vetoNew.getDisc() );
+      histograms_.fill("BDTDiscrLog", i+1 , -log(1-vetoNew.getDisc()) );
       histograms_.fill("RecoilPT", i+1 ,pT );
       histograms_.fill("RecoilPZ", i+1 , pZ );
       histograms_.fill("RecoilP", i+1 , totMom );
+      histograms_.fill("RecoilXAtTarget", i+1 ,XAtTarget );
+      histograms_.fill("RecoilPTAtTarget", i+1 ,pTAtTarget );
+      histograms_.fill("RecoilPZAtTarget", i+1 , pZAtTarget );
+      histograms_.fill("RecoilPAtTarget", i+1 , totMomAtTarget );
+      histograms_.fill("RecoilTheta", i+1. , thetaEleAtTarget );
+      histograms_.fill("RecoilPhi", i+1 , phiEleAtTarget );
       histograms_.fill("Hcal_MaxPE", i+1. , hcalMaxPE );
       histograms_.fill("Hcal_TotalPE", i+1. , hcalTotalPe );
       histograms_.fill("Hcal_MaxTiming", i+1. , hcalMaxTiming );
       histograms_.fill("Hcal_MaxSector", i+1. , hcalMaxSector );
+
+      if (i==1) {
+        histograms_.fill("BDTDiscrVsHcalPE_PreS", hcalMaxPE , vetoNew.getDisc() );
+        histograms_.fill("BDTDiscrLogVsHcalPE_PreS", hcalMaxPE , -log(1-vetoNew.getDisc()) );
+      } 
+      if (i==10) {
+        histograms_.fill("BDTDiscrVsHcalPE_PostS", hcalMaxPE , vetoNew.getDisc() );
+        histograms_.fill("BDTDiscrLogVsHcalPE_PostS", hcalMaxPE , -log(1-vetoNew.getDisc()) );
+      }
     }
   }
 
+// Alternative cutflow
+  // Alternative cutFlow here
+  bool passedCutsArray2[12];
+  std::fill(std::begin(passedCutsArray2), std::end(passedCutsArray2),false);
+  passedCutsArray2[0]  = ((fiducial_ && vetoNew.getRecoilX() !=-9999) || (!fiducial_ && vetoNew.getRecoilX() ==-9999)) ? true : false;
+  passedCutsArray2[1]  = (trigResult.passed()) ? true : false;
+  passedCutsArray2[2]  = (vetoNew.getSummedDet() < 3500) ? true : false;
+  passedCutsArray2[3]  = (vetoNew.getSummedTightIso() < 800) ? true : false;
+  passedCutsArray2[4]  = (vetoNew.getEcalBackEnergy() < 250) ? true : false;
+  passedCutsArray2[5]  = (vetoNew.getNReadoutHits() < 70) ? true : false;
+  passedCutsArray2[6]  = (vetoNew.getShowerRMS() < 110) ? true : false;
+  passedCutsArray2[7]  = (vetoNew.getYStd() < 70) ? true : false;
+  passedCutsArray2[8]  = (vetoNew.getMaxCellDep() < 300) ? true : false;
+  passedCutsArray2[9]  = (vetoNew.getStdLayerHit() < 5) ? true : false;
+  passedCutsArray2[10]  = (vetoNew.getNStraightTracks() < 6) ? true : false;
+  passedCutsArray2[11]  = (hcalVeto.passesVeto()) ? true : false;
+
+  for (size_t i=0;i<sizeof(passedCutsArray2);i++) {
+    bool allCutsPassedSoFar = true;
+    for (size_t j=0;j<=i;j++) {
+      if (!passedCutsArray2[j]) {
+        allCutsPassedSoFar = false;
+        break;
+      }
+    }
+    if (allCutsPassedSoFar) {
+      // std::cout 
+      //   << " i-th cut = " << i 
+      //   << " trigger = " << trigResult.passed() 
+      //   << " getRecoilX = " << vetoNew.getRecoilX() 
+      //   << " getSummedDet = " << vetoNew.getSummedDet()
+      //   << " getSummedTightIso = " << vetoNew.getSummedTightIso() 
+      //   << " getEcalBackEnergy = " << vetoNew.getEcalBackEnergy() 
+      //   << " getNReadoutHits = " << vetoNew.getNReadoutHits()
+      //   << " getShowerRMS = " << vetoNew.getShowerRMS()
+      //   << " getYStd = " << vetoNew.getYStd()
+      //   << " getMaxCellDep = " << vetoNew.getMaxCellDep()
+      //   << " getStdLayerHit = " << vetoNew.getStdLayerHit()
+      //   << " getNStraightTracks = " << vetoNew.getNStraightTracks()
+      //   << " hcalVeto = " << hcalVeto.passesVeto()
+      // std::cout << "hcalMaxPE = " <<  hcalMaxPE << " hcal total" << hcalTotalPe <<  " maxTime = " << hcalMaxTiming << " where = " << hcalMaxSector
+      // << std::endl;
+
+      histograms_.fill("AltCutFlow_RecoilX", i+1 , vetoNew.getRecoilX() );
+    }
+  }
   
    // Reverse cutflow, i.e. start with the last cut from the original cutflow
 histograms_.fill("Rev_AvgLayerHit", 0. , vetoNew.getAvgLayerHit() );
@@ -536,19 +703,19 @@ bool CutBasedDM::passPreselection(T (&passedCutsArray)[n], bool verbose) {
   }
 }
 
-std::tuple<int, const ldmx::SimParticle *> CutBasedDM::getRecoil(
+std::tuple<int, const ldmx::SimParticle *> CutBasedDM::getRecoilEle(
     const std::map<int, ldmx::SimParticle> &particleMap) {
   // The recoil electron is "produced" in the dark brem geneartion
   for (const auto &[trackID, particle] : particleMap) {
-    if (particle.getPdgID() == 11 and
-        particle.getProcessType() ==
-            ldmx::SimParticle::ProcessType::eDarkBrem) {
+    if (particle.getPdgID() == 11 and particle.getProcessType() == ldmx::SimParticle::ProcessType::eDarkBrem) {
       return {trackID, &particle};
-    }
+    }   
   }
-  // only get here if recoil electron was not "produced" by dark brem
-  //   in this case (bkgd), we interpret the primary electron as also the recoil
-  //   electron
+
+  // // only get here if recoil electron was not "produced" by dark brem
+  // //   in this case (bkgd), we interpret the primary electron as also the recoil
+  // //   electron
+  // ldmx::SimParticle::ProcessType::Primary
   return {1, &(particleMap.at(1))};
 }
 
